696da2b78987e880c095268dfc55662aec7040e3
Olivia Tang <tango2@uw.edu>
2018-03-01 16:13:47 -0800

    added base urls for census api

---
+++ main.R
@@ -0,0 +1,11 @@
+
+
+#####Olivia's work#######
+library("dplyr")
+library("httr")
+library("jsonlite")
+## for CPS poverty statistics
+poverty.status.url <- "https://api.census.gov/data/timeseries/poverty/histpov2"
+
+
+########################
